a978e02bb1e692177d283aa28932bdc649842ee9
Asim Shrestha <asim.shrestha@hotmail.com>
2020-09-04 17:20:42 -0700

    feat: add basic player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Player : MonoBehaviour {
+	[SerializeField] float movementSpeed = 7f;
+	[SerializeField] float jumpSpeed = 7f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private Rigidbody2D rb;
+	// Start is called before the first frame update
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		handleMovement();
+		handleJump();
+	}
+
+	private void handleMovement() {
+		Vector2 movementVector = new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed, rb.velocity.y);
+		rb.velocity = movementVector;
+	}
+
+	private void handleJump() {
+		if (Input.GetButtonDown("Jump")) {
+			Vector2 jumpVelocity = new Vector2(rb.velocity.x, jumpSpeed);
+			rb.velocity = jumpVelocity;
+		}
+	}
 }
