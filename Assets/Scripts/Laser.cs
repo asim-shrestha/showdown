909fae1a7c360bb82fa8f68eed9f2822c725f638
Asim Shrestha <asim.shrestha@hotmail.com>
2020-09-08 15:35:02 -0700

    feat: add laser base sprite

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Laser : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Laser : MonoBehaviour {
+	public LineRenderer lineRenderer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start() {
+		EnableLaser();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate() {
+		transform.Rotate(new Vector3(0, 0, 1));
+	}
+
+	public void DisableLaser() {
+		lineRenderer.enabled = false;
+	}
+
+	public void EnableLaser() {
+		lineRenderer.enabled = true;
+	}
 }
