d24f7b68b9dd401532487f3dfdd429aaccfd415b
Sean Nam <seannam@protonmail.com>
2020-09-08 06:13:09 -0700

    add explanations on new methods on player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,12 +10,13 @@ public class Player : NetworkBehaviour {
 	[SerializeField] float jumpSpeed = 7f;
 	[SerializeField] int maxJumps = 2;
 	[SerializeField] int jumpsAvailable;
-	[SerializeField] float wallRideTriggerSpeed = 0.1f; //upward direction
-	[SerializeField] float wallRideSpeed = -0.1f; //downward direction
+	[SerializeField] float wallslideTriggerSpeed = 0.1f; //upward direction
+	[SerializeField] float wallslideSpeed = -0.1f; //downward direction
 	
 	// GameObject instead of particle system to pass into a server command
 	[SerializeField] private GameObject dustParticles;
 	
+	// Ground and wall checks are preformed with the following gameobjects.
 	[SerializeField] private GameObject groundCheck;
 	private GroundCheck groundCheckScript;
 	[SerializeField] private GameObject LeftWallCheck;
@@ -61,15 +62,18 @@ public class Player : NetworkBehaviour {
 	void Update() {
 		if (hasAuthority) {
 			Debug.Log(GetJumpState());
+			// the order of these operations is important due to them changing the "JumpState" of the player.
 			handleLanding();
 			handleJump();
-			handleWallRide();
+			handleWallslide();
 			handleMovement();
 		}
 	}
 
 	private void handleLanding() {
 		if (GetJumpState() == GroundCheck.JumpState.LANDED) {
+			// As soon as landing has been detected, it should perform the following lines once
+			// and then immediately change the JumpState to ONGROUND, so these line won't run again.
 			jumpsAvailable = maxJumps;
 			CmdPlayDustParticles();
 			SetJumpState(GroundCheck.JumpState.ONGROUND);
@@ -89,6 +93,7 @@ public class Player : NetworkBehaviour {
 			Vector2 jumpVelocity = new Vector2(rb.velocity.x, jumpSpeed);
 			rb.velocity = jumpVelocity;
 			jumpsAvailable--;
+			// The following lines ensure that when the player falls off the edge and tries to jump, his available jumps decrements twice.
 			if (GetJumpState() == GroundCheck.JumpState.FALLING) {
 				jumpsAvailable--;
 				SetJumpState(GroundCheck.JumpState.INAIR);
@@ -96,14 +101,15 @@ public class Player : NetworkBehaviour {
 		}
 	}
 
-	private void handleWallRide() {
+	private void handleWallslide() {
+		// if player is moving left onto a left wall or moving right onto a right wall, wall sliding will activate.
 		if ((GetIsOnLeftWall() && Input.GetAxisRaw("Horizontal") < 0) ||
 			(GetIsOnRightWall() && Input.GetAxisRaw("Horizontal") > 0)) {
-			if (rb.velocity.y < wallRideTriggerSpeed) {
+			if (rb.velocity.y < wallslideTriggerSpeed) {
 				SetJumpState(GroundCheck.JumpState.LANDED);
 				CmdPlayDustParticles();
-				if (rb.velocity.y < wallRideSpeed) {
-					Vector2 slideVelocity = new Vector2(rb.velocity.x, wallRideSpeed);
+				if (rb.velocity.y < wallslideSpeed) {
+					Vector2 slideVelocity = new Vector2(rb.velocity.x, wallslideSpeed);
 					rb.velocity = slideVelocity;
 				}
 			}
