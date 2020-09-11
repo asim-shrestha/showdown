63bcba5461e74a2786c13da2b05102c75bcc6abc
Sean Nam <seannam@protonmail.com>
2020-09-11 08:24:54 -0700

    make grenades blink

--- Assets/Scripts/Grenade.cs
+++ Assets/Scripts/Grenade.cs
@@ -5,27 +5,38 @@ using UnityEngine;
 public class Grenade : MonoBehaviour
 {
 	[SerializeField] float timeToExplode;
+	[SerializeField] float blinkDuration;
 	[SerializeField] float randomBounceThrust;
 	[SerializeField] float randomBounceStopSpeedThreshold;
+	
+	float timeLeftToExplode;
+	float blinkInterval;
 	private float currentThrust;
 	private bool isBlinking;
+	private float EPSILON = 0.05f;
 
 	private Rigidbody2D rb;
 	private SpriteRenderer sr;
 
 	Color grenadeColor;
 	Color warningColor;
+	Color flashColor;
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		grenadeColor = new Color(25, 145, 0);
-		warningColor = new Color(168, 9, 2);
+		grenadeColor = new Color(0.1f, 0.6f, 0f, 1f);
+		warningColor = new Color(0.7f, 0.1f, 0f, 1f);
+		flashColor = new Color(1f, 1f, 1f, 1f);
 
 		rb = GetComponent<Rigidbody2D>();
 		sr = GetComponent<SpriteRenderer>();
 		Vector2 direction = new Vector2(Random.Range(-3f,3f), 0);
 		rb.AddForce(direction * 4f, ForceMode2D.Impulse);
+
+		sr.color = grenadeColor;
+		timeLeftToExplode = timeToExplode;
+		blinkInterval = timeToExplode;
 	}
 
 	// Update is called once per frame
@@ -37,28 +48,31 @@ public class Grenade : MonoBehaviour
 			currentThrust = randomBounceThrust;
 		}
 
-		timeToExplode -= Time.deltaTime;
-		if (timeToExplode <= 0) { 
+		// Destroy game object when time is up. Proceed to exploding.
+		timeLeftToExplode -= Time.deltaTime;
+		if (timeLeftToExplode <= 0) { 
 			Destroy(gameObject);
 		}
-		if (timeToExplode < 1 && !isBlinking) {
-			StartCoroutine(ChangeColor(0.25f));
-			return;
-		}
-		if (timeToExplode < 3 && !isBlinking) {
-			StartCoroutine(ChangeColor(0.5f));
-			return;
+
+		changeBlinkInterval(5f, 0.6f);
+		changeBlinkInterval(2f, 0.15f);
+		
+		if (timeLeftToExplode % blinkInterval > -EPSILON && timeLeftToExplode % blinkInterval < EPSILON) {
+			StartCoroutine(blink());
 		}
 	}
 
-	IEnumerator ChangeColor(float interval) {
-		// Your code here…
+	void changeBlinkInterval(float timeLeft, float interval) {
+		if (timeLeftToExplode < timeLeft) {
+			blinkInterval = interval;
+		}
+	}
+
+	IEnumerator blink() {
 		sr.color = warningColor;
-		yield return new WaitForSeconds(interval);
+		yield return new WaitForSeconds(blinkDuration);
 		sr.color = grenadeColor;
-		yield return new WaitForSeconds(interval);
 	}
-
 
 	void OnCollisionEnter2D(Collision2D other) {
 		Vector2 direction = new Vector2(Random.Range(-3f,3f), Random.Range(0f,1f));
